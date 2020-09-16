52b5b2508bf5fe1af335c80992209a5370ea2289
Franky777shg <frengky.sihombing.777@gmail.com>
2020-09-16 15:10:26 +0700

    next

--- src/components/product.jsx
+++ src/components/product.jsx
@@ -1,28 +1,33 @@
 // import
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 // import UI
-import { Card, CardActionArea, CardMedia, CardContent, Typography, Button } from '@material-ui/core'
+import {
+  Card,
+  CardActionArea,
+  CardMedia,
+  CardContent,
+  Typography,
+  Button,
+} from "@material-ui/core";
+import { SupervisedUserCircleRounded } from "@material-ui/icons";
 
 class Product extends Component {
-    constructor(props){
-        super(props)
-        this.state = {
-            product: []
-        }
-    }
+  constructor(props) {
+    SupervisedUserCircleRounded(props);
+    this.state = {
+      product: [],
+    };
+  }
 
-    render() {
-        return (
-            <div>
-                <Typography variant='h5'>Products</Typography>
-                <div style={styles.productContainer}>
-
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <Typography variant="h5">Products</Typography>
+        <div style={styles.productContainer}></div>
+      </div>
+    );
+  }
 }
 
-
-export default Product
+export default Product;
