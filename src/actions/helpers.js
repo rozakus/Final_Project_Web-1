ba6de6a76c36aa659aea6915c183cbafdd689ad0
adenugrah25 <adenugrah25@gmail.com>
2020-09-17 23:56:22 +0700

    profile-page

--- src/actions/helpers.js
+++ src/actions/helpers.js
@@ -1,7 +1,10 @@
 export const URL = 'http://localhost:2222/';
+export const URL_IMG = 'http://localhost:2000/'
 
 // user
 export const LOGIN = 'LOGIN'
 export const LOG_OUT = 'LOG_OUT'
-
 export const SIGN_UP = 'SIGN_UP'
+
+// profile
+export const GET_PROFILE = 'GET_PROFILE'
