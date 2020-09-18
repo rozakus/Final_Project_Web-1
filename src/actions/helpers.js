ccd607197aadc55dbff51802157af39ffec81497
adenugrah25 <adenugrah25@gmail.com>
2020-09-18 16:34:39 +0700

    register in frontend without nodemailer

--- src/actions/helpers.js
+++ src/actions/helpers.js
@@ -4,11 +4,10 @@ export const URL_IMG = 'http://localhost:2000/'
 // user
 export const LOGIN = 'LOGIN'
 export const LOG_OUT = 'LOG_OUT'
-export const SIGN_UP = 'SIGN_UP'
+export const REGISTER = 'REGISTER'
 
 // profile
 export const GET_PROFILE = 'GET_PROFILE'
-export const SIGN_UP = 'SIGN_UP'
 
 // product
 export const GET_PRODUCT = 'GET_PRODUCT'
