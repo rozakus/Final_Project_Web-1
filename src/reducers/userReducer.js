564b565c4120bd9e408e0b7455a443ee878fd912
adenugrah25 <adenugrah25@gmail.com>
2020-09-23 11:13:11 +0700

    repair localstorage of Logout

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,43 +1,46 @@
-import { LOGIN, LOGIN_ERROR, REGISTER, LOG_OUT } from '../actions'
-const INITIAL_STATE = { //harus sama yg di postman
-    id : null,
-    username : '',
-    email : '',
-    role : '',
-    register_status: false,
-    address : '',
-    picture : '',
-    errorMsg: ''
-}
+import { LOGIN, LOGIN_ERROR, REGISTER, LOG_OUT } from "../actions";
+const INITIAL_STATE = {
+  //harus sama yg di postman
+  id: null,
+  username: "",
+  email: "",
+  role: "",
+  register_status: false,
+  address: "",
+  picture: "",
+  errorMsg: "",
+  
+};
 
 export const userReducer = (state = INITIAL_STATE, action) => {
-    switch(action.type) {
-        case LOGIN :
-            return { //harus sama yg di postman
-                ...state,
-                id : action.payload.id_users,
-                username : action.payload.username,
-                email : action.payload.email,
-                role : action.payload.role,
-                address : action.payload.address,
-                picture : action.payload.picture
-            }
-        case REGISTER :
-            return {...state,
-                id : action.payload.id,
-                username : action.payload.username,
-                email : action.payload.email,
-                role : action.payload.role
-            }
-        case LOGIN_ERROR :
-            return {
-                ...state,
-                errorMsg : action.payload
-
-            }
-        case LOG_OUT :
-        return INITIAL_STATE
-        default : 
-        return state
-    }
-}
+  switch (action.type) {
+    case LOGIN:
+      return {
+        //harus sama yg di postman
+        ...state,
+        id: action.payload.id_users,
+        username: action.payload.username,
+        email: action.payload.email,
+        role: action.payload.role,
+        address: action.payload.address,
+        picture: action.payload.picture,
+      };
+    case REGISTER:
+      return {
+        ...state,
+        id: action.payload.id,
+        username: action.payload.username,
+        email: action.payload.email,
+        role: action.payload.role,
+      };
+    case LOGIN_ERROR:
+      return {
+        ...state,
+        errorMsg: action.payload,
+      };
+    case LOG_OUT:
+      return INITIAL_STATE;
+    default:
+      return state;
+  }
+};
